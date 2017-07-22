9583ab12ccee3f2eb7810e741339c5855c8b82c7
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-07-22 14:16:26 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char string1,string;
+scanf(%s",&string);
+string1=strrer(string);
+if(string==string1)
+printf("palindrome");
+else
+printf("not palindrome");
+}
