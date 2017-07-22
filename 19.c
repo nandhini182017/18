29175ff0668dc76fd36d81580ffbf3efb8badddc
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-07-22 16:17:15 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int i=1,n,fact=1;
+printf(enter the value of n");
+scanf("%d",&n)
+while(i>=n)
+{
+fact=fact*i
+i=i+1
+}
+printf("the factorial of n%dis%d",n);
+}
+
+
+
+
+
+
+
+
+
+'
